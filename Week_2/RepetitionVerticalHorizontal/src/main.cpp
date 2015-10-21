60ddbe613300392156b30bdebe8ac68dfc56280e
Christopher Baker <me@christopherbaker.net>
2015-10-21 08:26:28 -0500

    Week 2 0.9.0 updates.

--- Week_2/RepetitionVerticalHorizontal/src/main.cpp
+++ Week_2/RepetitionVerticalHorizontal/src/main.cpp
@@ -1,42 +1,43 @@
-#line 1 "RepetitionVerticalHorizontal"
-
 #include "ofMain.h"
 
 
 class ofApp: public ofBaseApp
 {
 public:
+	void setup()
+	{
+		ofSetWindowShape(512, 512); // Set the initial window size.
+		ofBackground(0); // Set the background to 0 (black).
+	}
 
-#line 1 "RepetitionVerticalHorizontal"
-void setup() {
-    ofSetWindowShape(512, 512); // Set the initial window size.
-    ofBackground(0); // Set the background to 0 (black).
-}
+	void draw()
+	{
+		// Set the drawing color to white.
+		ofSetColor(255);
 
-void draw() {
-    // Set the drawing color to white.
-    ofSetColor(255);
+		// Draw vertical lines every 5 pixels while 'x' is less than the window width.
+		for (int x = 0; x < ofGetWidth(); x += 5)
+		{
+			ofDrawLine(x, 0, x, ofGetHeight());
+		}
 
-    // Draw vertical lines every 5 pixels while 'x' is less than the window width.
-    for (int x = 0; x < ofGetWidth(); x += 5)
-    {
-	    ofLine(x, 0, x, ofGetHeight());
-    }
-    
-    // Draw horizontal lines every 5 pixels while 'y' is less than the window height.
-    for (int y = 0; y < ofGetHeight(); y += 5)
-    {
-	    ofLine(0, y, ofGetWidth(), y);
-    }
-    
-}
+		// Draw horizontal lines every 5 pixels while 'y' is less than the window height.
+		for (int y = 0; y < ofGetHeight(); y += 5)
+		{
+			ofDrawLine(0, y, ofGetWidth(), y);
+		}
 
+	}
 
 };
 
+
 int main()
 {
-    ofSetupOpenGL(320, 240, OF_WINDOW);
-    ofRunApp(new ofApp());
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3, 2);
+	settings.width = 1024;
+	settings.height = 768;
+	ofCreateWindow(settings);
+	ofRunApp(std::make_shared<ofApp>());
 }
-

34cb664870192a0022098a870346452434dab541
Christopher Baker <me@christopherbaker.net>
2016-09-30 07:34:00 -0500

    Update examples.

---
+++ Week_04/03_PolylineBasic/src/ofApp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include "ofMain.h"
+
+
+class ofApp: public ofBaseApp
+{
+public:
+	void setup() override;
+	void update() override;
+    void draw() override;
+
+    void mousePressed(int x, int y, int mouse) override;
+    void mouseDragged(int x, int y, int mouse) override;
+    void keyPressed(int key) override;
+
+    ofPolyline polyline;
+
+    float position = 0;
+
+};

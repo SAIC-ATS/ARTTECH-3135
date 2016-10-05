30b28852d47371530dbf49241b4f98a80eb240d7
Christopher Baker <me@christopherbaker.net>
2016-10-05 14:20:35 -0500

    Basic particles.

---
+++ Week_05/01_ParticleSimple/src/Particle.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ofMain.h"
+
+class Particle
+{
+public:
+    void setup();
+    void update();
+    void draw();
+
+    ofVec3f previousPosition;
+
+    ofVec3f position;
+    ofVec3f velocity;
+    ofVec3f acceleration;
+    float drag = 0.98;
+    uint64_t age = 0;
+
+};

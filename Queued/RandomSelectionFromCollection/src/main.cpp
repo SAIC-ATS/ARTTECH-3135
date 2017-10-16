9350ff657b9284a865dd537e49add0489e2361f9
Christopher Baker <me@christopherbaker.net>
2017-10-16 14:11:33 -0500

    Remove long paths.

--- Queued/RandomSelectionFromCollection/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "ofApp.h"
-
-int main()
-{
-	ofSetupOpenGL(512, 512, OF_WINDOW);
-	return ofRunApp(std::make_shared<ofApp>());
-}
--- Queued/Week_07/ParticleModelMesh/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ofApp.h"
-
-
-int main()
-{
-	ofSetupOpenGL(1024, 768, OF_WINDOW);
-    ofRunApp(std::make_shared<ofApp>());
-}

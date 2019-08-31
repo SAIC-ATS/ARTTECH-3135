c6836a288d2fddaf87623a9ef084181aa304ffc4
Christopher Baker <me@christopherbaker.net>
2019-08-30 20:30:43 -0500

    Fix main.

--- Session_00/Mondrian/src/main.cpp
+++ Session_00/Mondrian/src/main.cpp
@@ -3,10 +3,9 @@
 
 int main()
 {
-    ofGLWindowSettings settings;
+    ofWindowSettings settings;
     // 770 x 775 is the size of our Mondrian image.
-    settings.width = 770;
-    settings.height = 775;
+    settings.setSize(770, 775);
     ofCreateWindow(settings);
     return ofRunApp(std::make_shared<ofApp>());
 }
